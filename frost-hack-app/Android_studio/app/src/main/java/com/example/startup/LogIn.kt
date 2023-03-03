cc58bc47f17291f3ddbc8502bc176109a81bf48e
Saransh Baniyal <saranshbaniyal14@gmail.com>
2023-03-04 04:03:45 +0530

    App: SignUp and Login

--- frost-hack-app/Android_studio/app/src/main/java/com/example/startup/LogIn.kt
+++ frost-hack-app/Android_studio/app/src/main/java/com/example/startup/LogIn.kt
@@ -1,6 +1,9 @@
 package com.example.startup
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -8,11 +11,18 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.startup.databinding.ActivityLogInBinding
+import okhttp3.*
+import okio.IOException
+import org.json.JSONObject
+import org.json.JSONTokener
 
 class LogIn : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityLogInBinding
+    private lateinit var edusername: EditText
+    private lateinit var edpassword: EditText
+    private lateinit var btLogin: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,21 +30,53 @@ class LogIn : AppCompatActivity() {
         binding = ActivityLogInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        edpassword = findViewById(R.id.edusername)
+        edpassword = findViewById(R.id.edpassword)
+        btLogin = findViewById(R.id.btLogin)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_log_in)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        btLogin.setOnClickListener {
+            val username = edusername.text.toString()
+            val password = edpassword.text.toString()
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            logInFunc("johndoe","mypassword")
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_log_in)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+
+    private fun logInFunc(username: String, password: String) {
+        val client = OkHttpClient()
+
+        val requestBody = FormBody.Builder()
+            .add("username", username)
+            .add("password", password)
+            .build()
+
+        val headers = Headers.Builder()
+//            .add("Authorization", "Bearer <your_token>")
+            .add("Content-Type", "application/json")
+            .add("ngrok-skip-browser-warning", "abc")
+            .build()
+
+        val request = Request.Builder()
+            .url(" https://3ac5-152-58-108-241.in.ngrok.io/api/accounts/login/")
+            .post(requestBody)
+            .headers(headers)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                // Handle successful response here
+                val jsonObject = JSONTokener(response.body.toString()).nextValue() as JSONObject
+                val success = jsonObject.getString("Success")
+                if (success.equals("True"))
+                    Toast.makeText(baseContext, "Success", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                // Handle error response here
+            }
+        })
+
     }
+
 }
--- frost-hack-app/Android_studio/app/src/main/java/com/example/startup/SignUp.kt
+++ frost-hack-app/Android_studio/app/src/main/java/com/example/startup/SignUp.kt
@@ -1,18 +1,24 @@
 package com.example.startup
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.startup.databinding.ActivitySignUpBinding
+import okhttp3.*
+import okio.IOException
 
 class SignUp : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivitySignUpBinding
+    private lateinit var edUserName: EditText
+    private lateinit var edemail: EditText
+    private lateinit var edpassword: EditText
+    private lateinit var edfull_name: EditText
+    private lateinit var btSignUp: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,21 +26,55 @@ class SignUp : AppCompatActivity() {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        edUserName= findViewById(R.id.edUserName)
+        edfull_name= findViewById(R.id.edfull_name)
+        edemail= findViewById(R.id.edemail)
+        edpassword= findViewById(R.id.edpassword)
+        btSignUp= findViewById(R.id.btSignUp)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_sign_up)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        btSignUp.setOnClickListener {
+            val username=edUserName.text.toString()
+            val full_name=edfull_name.text.toString()
+            val email=edemail.text.toString()
+            val password=edpassword.text.toString()
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            signUpFunc(username,full_name,email,password);
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_sign_up)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+    private fun signUpFunc(username: String, full_name: String, email: String, password: String){
+        val client = OkHttpClient()
+
+        val requestBody = FormBody.Builder()
+            .add("username", username)
+            .add("full_name", full_name)
+            .add("email", email)
+            .add("password", password)
+            .build()
+
+        val headers = Headers.Builder()
+//            .add("Authorization", "Bearer <your_token>")
+            .add("Content-Type", "application/json")
+            .add("ngrok-skip-browser-warning", "abc")
+            .build()
+
+        val request = Request.Builder()
+            .url(" https://3ac5-152-58-108-241.in.ngrok.io/api/accounts/signup/")
+            .post(requestBody)
+            .headers(headers)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                // Handle successful response here
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                // Handle error response here
+            }
+        })
+
     }
+
+
 }
